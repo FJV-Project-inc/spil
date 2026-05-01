caff86f01b31b1bf0bc55f05680dad767ce14756
viggevig <viggevild@gmail.com>
2026-05-01 14:04:36 +0200

    alt for skoletiden

--- programmerings-projekt-spil/pointsystem.cs
+++ programmerings-projekt-spil/pointsystem.cs
@@ -1,6 +1,6 @@
 using Godot;
 using System;
-
+int points = 0;
 public partial class pointsystem : TextEdit
 {
 	// Called when the node enters the scene tree for the first time.
@@ -9,8 +9,15 @@ public partial class pointsystem : TextEdit
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _Input(InputEvent @event){
+	if (@event is InputEventKey key && key.Pressed)
 	{
-		
+		if (key.Keycode == Key.D)
+		{
+			
+			points = player.position.x
+			GD.Print(points);
+		}
 	}
+}
 }
